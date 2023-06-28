a161cde42d38acd1082784c64396da5d45c03c86
Daniele Ceribelli <danieleceribelli@gmail.com>
2023-06-28 23:09:42 +0200

    Add code to test database (it will be remove later)

--- app/src/main/java/com/example/twit_tok/presentation/Profile/ProfileFragment.java
+++ app/src/main/java/com/example/twit_tok/presentation/Profile/ProfileFragment.java
@@ -1,6 +1,7 @@
 package com.example.twit_tok.presentation.Profile;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,10 +11,14 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.twit_tok.data.db.TwokDatabase;
 import com.example.twit_tok.databinding.FragmentProfileBinding;
+import com.example.twit_tok.domain.Sid;
+import com.google.common.util.concurrent.ListenableFuture;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class ProfileFragment extends Fragment {
 
@@ -23,7 +28,24 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
+
+        Log.d("TEST", getActivity().getApplicationContext().toString());
+        TwokDatabase t = TwokDatabase.getInstance(getActivity().getApplicationContext());
+        ListenableFuture<Sid> s = t.getSidDao().getSid();
+        s.addListener(() -> {
+            try {
+                Sid sid = s.get();
+                System.out.println(sid);
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, getActivity().getMainExecutor());
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+/*
         ProfileViewModel profileViewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
+
+
         // l = (ListView) binding.getRoot().getViewById(R.id.followedList);
         List<String> myList = new ArrayList<>();
 
@@ -34,10 +56,7 @@ public class ProfileFragment extends Fragment {
         myList.add("bene");
         myList.add("e");
         myList.add("tu");
-
-
-        binding = FragmentProfileBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+*/
 
         return root;
     }
