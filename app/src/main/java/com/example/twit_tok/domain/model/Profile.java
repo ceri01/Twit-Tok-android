0c3f66fad6b5b3c862fe4c1f0cbe69ae1ec33ee4
Daniele Ceribelli <danieleceribelli@gmail.com>
2023-07-02 15:53:36 +0200

    Add profile model and turn Sid into a class

--- app/src/main/java/com/example/twit_tok/domain/model/Profile.java
+++ app/src/main/java/com/example/twit_tok/domain/model/Profile.java
@@ -1,2 +1,51 @@
-package com.example.twit_tok.domain.model;public class Profile {
+package com.example.twit_tok.domain.model;
+
+import androidx.annotation.NonNull;
+
+import java.util.Objects;
+
+public class Profile {
+    private final int uid;
+    private final String name;
+    private final String picture;
+    private final int pversion;
+
+    public Profile(int uid, String name, String picture, int pversion) {
+        Objects.requireNonNull(name, "Name cannot be null");
+        if (name.isBlank()) {
+            this.name = "unnamed";
+        } else {
+            this.name = name;
+        }
+        this.picture = picture;
+        this.pversion = pversion;
+        this.uid = uid;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", picture='" + picture + '\'' +
+                ", uid=" + uid +
+                ", pversion=" + pversion +
+                '}';
+    }
+
+    public int uid() {
+        return uid;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String picture() {
+        return picture;
+    }
+
+    public int pversion() {
+        return pversion;
+    }
 }
--- app/src/main/java/com/example/twit_tok/domain/model/Sid.java
+++ app/src/main/java/com/example/twit_tok/domain/model/Sid.java
@@ -1,9 +1,26 @@
 package com.example.twit_tok.domain.model;
 
+import androidx.annotation.NonNull;
+
 import java.util.Objects;
 
-public record Sid(String sid) {
-    public Sid {
+public final class Sid {
+    private final String sid;
+
+    public Sid(String sid) {
         Objects.requireNonNull(sid, "Sid cannot be null");
+        this.sid = sid;
     }
+
+    public String sid() {
+        return sid;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "Sid[" +
+                "sid=" + sid + ']';
+    }
+
 }
