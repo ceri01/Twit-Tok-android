0d473f02a1c2ae21ea0f02d01437f14eeaf42beb
Daniele Ceribelli <danieleceribelli@gmail.com>
2023-08-10 12:55:05 +0200

    Set default fragment to display and remove defaultNavHost

--- app/src/main/java/com/example/twit_tok/MainActivity.java
+++ app/src/main/java/com/example/twit_tok/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.twit_tok;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.twit_tok.databinding.ActivityMainBinding;
+
+import java.util.Objects;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
@@ -33,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-
+        if (Objects.isNull(savedInstanceState)) {
+            navView.setSelectedItemId(R.id.navigation_wall);
+        }
     }
 }
