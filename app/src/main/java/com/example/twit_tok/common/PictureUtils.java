69558ab45b4847f9bdf90478912702010f6e8ed4
Daniele Ceribelli <danieleceribelli@gmail.com>
2023-08-29 16:17:44 +0200

    Add regex to exclude some wrong pictures and fix isValidPicture

--- app/src/main/java/com/example/twit_tok/common/PictureUtils.java
+++ app/src/main/java/com/example/twit_tok/common/PictureUtils.java
@@ -3,6 +3,8 @@ package com.example.twit_tok.common;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.service.autofill.FieldClassification;
+import android.util.Log;
 
 import androidx.core.content.res.ResourcesCompat;
 
@@ -15,9 +17,11 @@ import java.util.regex.Pattern;
 
 public class PictureUtils {
     public static boolean isValidFormat(String s) {
-        Pattern pattern = Pattern.compile("^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$");
-        Matcher matcher = pattern.matcher(s);
-        return matcher.matches();
+        Pattern patternPath = Pattern.compile("^.*\\\\.[a-zA-Z0-9]+$");
+        Pattern patternBase64 = Pattern.compile("^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$");
+        Matcher matcherPath = patternPath.matcher(s);
+        Matcher matcherBase64 = patternBase64.matcher(s);
+        return matcherBase64.matches() && !matcherPath.matches();
     }
 
     public static boolean isValidDimension(String s) {
@@ -25,7 +29,7 @@ public class PictureUtils {
     }
 
     public static boolean isValidPicture(String s) {
-        return !Objects.isNull(s) && isValidDimension(s) && isValidDimension(s);
+        return !Objects.isNull(s) && isValidFormat(s) && isValidDimension(s);
     }
 
     public static Bitmap getDefaultPicture() {
