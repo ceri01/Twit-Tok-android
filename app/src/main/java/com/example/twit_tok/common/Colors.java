835cf426724f0ceffcbf181f44ed9b9121b84ad1
Daniele Ceribelli <danieleceribelli@gmail.com>
2023-08-30 16:54:19 +0200

    Add check in isValid

--- app/src/main/java/com/example/twit_tok/common/Colors.java
+++ app/src/main/java/com/example/twit_tok/common/Colors.java
@@ -2,11 +2,13 @@ package com.example.twit_tok.common;
 
 import android.graphics.Color;
 
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Colors {
     public static boolean isValid(String color) {
+        if (Objects.isNull(color)) return false;
         Pattern pattern = Pattern.compile("^[0-9A-Fa-f]{6}$");
         Matcher matcher = pattern.matcher(color);
         return matcher.matches();
